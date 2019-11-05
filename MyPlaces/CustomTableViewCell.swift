a044a53c706ee9ba7bb7ecb8607b4efb0e0c3b34
Aro797919 <aro797919@gmail.com>
2019-11-05 15:16:28 +0300

    Add view name, location, type

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Aro on 05.11.2019.
+//  Copyright © 2019 Aro. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: NSLayoutConstraint!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
